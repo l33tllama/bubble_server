8cb7fda41c1d9101a60d24738dfd69dd109b058d
Shane Pemmelaar <Shane Pemmelaar>
2016-07-31 16:17:48 +1000

    added use statements

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ var express = require('express');
 var app = express();
 var path = require('path');
 
+app.use(express.static('css'));
+app.use(express.static('img'))
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/index.html'));
 });
